c975f14073e916186391c3318c7b19926483343d
WizAom <Aom_ll2555@hotmail.com>
2021-01-12 00:41:12 +0700

    DEAD,READY TO W

--- src/App.js
+++ src/App.js
@@ -1,13 +1,124 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import CourseCard from "./components/CourseCard";
 import CourseForm from "./components/CourseForm";
+import { useContext, useState } from "react";
+
+export const sample = createContext()
 
 function App() {
+  const [myCourses, setMyCourse] = useState([]);
+  const [GPA, setGPA] = useState(4.0);
+  useEffect(() => {
+    const temp = localStorage.getItem('f')
+    if(temp!=null){
+      setMyCourse(JSON.parse(temp))
+      calculateGPA(JSON.parse(temp))
+    }
+  },[])
+  useEffect(() => {
+    localStorage.setItem('f',JSON.stringify(myCourses))
+  },[myCourses]) 
+  /**
+   * Calculate the GPA of current courses
+   * @returns the GPA of current courses
+   */
+  function calculateGPA(cc) {
+    // TODO
+    var r_gpa = 0
+    var r_cre  = 0 
+    var cal_gpa = 0
+    cc.forEach((item) => {
+      switch(item.grd){
+        case 'A' :
+          r_gpa = 4
+          r_cre += Number(item.crd) 
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'B+' :
+          r_gpa = 3.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'B' :
+          r_gpa = 3
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'C+' :
+          r_gpa = 2.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'C' :
+          r_gpa = 2
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'D+' :
+          r_gpa = 1.5
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'D' :
+          r_gpa = 1
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+        case 'F' :
+          r_gpa = 0
+          r_cre += Number(item.crd)
+          cal_gpa += r_gpa * Number(item.crd)
+          break
+      }  
+    });
+    setGPA(cal_gpa / r_cre) 
+  }
+
+  /**
+   * Should be called when a course is to be added to the list.
+   * After adding the course to the list, the displayed GPA should be updated.
+   * @param {*} event 
+   */
+  function addCourse(inputData) {
+    console.log(inputData)
+    // TODO
+    const course = [...myCourses,inputData]
+    setMyCourse(course)
+    // recalculate GPA
+    calculateGPA(course);
+  }
+
+  /**
+   * Should be called when a course is to be removed from the list.
+   * After removing the course from the list, the displayed GPA should be updated.
+   * @param {*} id 
+   */
+  function onDeleteCourse(id) {
+    // TODO
+    const course = myCourses.filter(item => {
+      return item.name !== id
+    })
+    setMyCourse(course)
+    calculateGPA(course)
+  }
+
   return (
-    <div>
-      <h1>GPA CALCULATOR</h1>
-      {/* TODO ADD UI */}
+    <sample.Provider value = {addCourse}>
+      <div className="bg-purple-300 container mx-auto h-screen">
+      <h1 className="bg-yellow-400 text-left text-10x10 p-7 tracking-widest">GPA CALCULATOR </h1>
+      <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
+      <h1 className="bg-pink-400  rounded-3xl p-1 text-center text-2xl my-3">My courses</h1>
+        {/* TODO display courses */}
+        {myCourses.map(item => {
+          return <CourseCard name ={item.name} grd = {item.grd} crd ={item.crd} del={onDeleteCourse} />
+        })}
+      <CourseForm /> 
+      </div>
+      {/* TODO add course input form */}
+      {/* TODO display calculated GPA */}
+      <p className = "bg-green-500 rounded-2xl p-2 text-center "> gpa is {GPA}</p>
     </div>
+  </sample.Provider>
   );
 }
 
